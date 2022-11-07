16e9397f2261c3d39067d1ef40a3655d3f51433d
abuh1 <abuhamza2308@hotmail.com>
2022-11-07 14:42:11 +0000

    update

--- src/todoList/ToDo.java
+++ src/todoList/ToDo.java
@@ -4,40 +4,34 @@ import java.util.Scanner;
 import java.util.Iterator;
 
 public class ToDo {
-	public static Scanner userInput = new Scanner(System.in);
 	public static ArrayLinearList arrayList = new ArrayLinearList();
 	
 	public static void main(String[] args) {
-		int menuNumber  = -1;
 		
+		int menuNumber  = -1;
 		while(menuNumber != 0) {
 			menuNumber = menu();
 			
 			switch(menuNumber) {
-			
-			case 0:
-				break;
-			
 			case 1:
 				arrayList.outputList();
 				break;
-				
 			case 2:
 				addTodo();
 				break;
-				
 			case 3:
 				removeTodo();
 				break;
-				
 			case 4:
 				getSize();
 				break;
-				
 			case 5:
 				searchTodo();
 				break;
-			
+			case 0:
+				break;
+			default:
+				System.out.println("Enter a valid option");
 			}
 			
 		}
@@ -45,12 +39,8 @@ public class ToDo {
 	}
 	
 	public static int menu() {
-		int choice;
-		
 		Scanner input = new Scanner(System.in);
 		System.out.println("\nMain Menu");
-		System.out.println();
-		System.out.println("Please type a choice: ");
 		System.out.println();
 		System.out.println("0. Exit the program\n"
 				+ "1. Display the todo\n"
@@ -59,15 +49,19 @@ public class ToDo {
 				+ "4. Display the number of todos in the list\n"
 				+ "5. Find a todo by name\n"
 				+ "6. Check if a todo is in the list\n");
-		choice = input.nextInt();
+		System.out.println();
+		System.out.println("Please type a choice: ");
+		int choice = input.nextInt();
 		return choice;
 	}
 	
 	public static void addTodo() {
+		System.out.println("Add Item");
+		System.out.println("---------------------------------------");
 		System.out.println("Please type your todo:");
-		
 		Scanner addtodo = new Scanner(System.in);
-		arrayList.add(arrayList.size(), addtodo);
+		String todo = addtodo.nextLine();
+		arrayList.add(arrayList.size(), todo);
 	}
 	
 	public static void removeTodo() {
